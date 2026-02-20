bc8c0a27d340483aff8a09d23a0cf1b642335cf6
Esmailiiaub-ai <esmailiiaub@gmail.com>
2026-02-20 09:52:53 +0330

    Create server configuration file

---
+++ server/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Server configuration
+
+// Current Date and Time
+const CURRENT_DATE_TIME = '2026-02-20 06:13:51';
+
+// Current User's Login
+const CURRENT_USER_LOGIN = 'Esmailiiaub-ai';
+
+?>
